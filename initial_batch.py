f20cdae099a5842707d7f4eea113f48121232c3f
as2811-project <anandh.tedxchennai@gmail.com>
2024-07-17 17:45:14 +1000

    Add documentation

--- initial_batch.py
+++ initial_batch.py
@@ -1,11 +1,24 @@
-## Australian Energy Production Data Pipeline
 import pandas as pd
 import requests
 import json
 import os
 from sqlalchemy import create_engine
 
-api_url = "https://visualisations.aemo.com.au/aemo/apps/api/report/5MIN"
+"""
+    Fetches electricity data from an API, processes it, and stores it in a database.
+
+    This function sends a POST request to the specified API URL with a payload containing the desired time scale.
+    The response is then converted to JSON format and processed using pandas.
+    The processed data is stored in a database table for each unique state in the 'REGION' column.
+
+    Parameters:
+        None
+
+    Returns:
+        None
+"""
+
+api_url = os.getenv("API_URL")
 payload = json.dumps({"timeScale":["5MIN"]})
 headers = {'Content-Type':'application/json'}
 
